98722fc53657b83ea00643e4329e33911349cdf5
tmartinelli <thiagov.martinelli@gmail.com>
2015-07-14 00:29:46 -0300

    Add feature tests for submit guess

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -3,11 +3,24 @@ end
 
 When /^I start a new game$/ do
   game = Codebreaker::Game.new(output_obj)
-  game.start
+  game.start("1234")
 end
 
 Then /^I should see "([^"]*)"$/ do |message|
   expect(output_obj.messages).to include(message)
+end
+
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  @game = Codebreaker::Game.new(output_obj)
+  @game.start(secret)
+end
+
+When /^I guess "([^"]*)"$/ do |guess|
+  @game.guess = guess
+end
+
+Then /^the mark should be "([^"]*)"$/ do |mark|
+  expect(output_obj.messages).to include(mark)
 end
 
 class Output
